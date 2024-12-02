47bd2581784e480bf33ca8c8e2a39952ecee5896
bec23 <136933467+bec23@users.noreply.github.com>
2024-12-02 12:59:57 -0600

    Update class.py

--- class.py
+++ class.py
@@ -1,3 +1,4 @@
+#animal class
 class Animal:
     def __init__(self, id, name, species, age, health):
         self.id = id
@@ -14,7 +15,7 @@ class Animal:
             'age': self.age,
             'health': self.health
         }
-
+#adopter class
 class Adopter:
     def __init__(self, id, name, contact):
         self.id = id
@@ -27,7 +28,7 @@ class Adopter:
             'name': self.name,
             'contact': self.contact
         }
-
+#adpotion record class
 class AdoptionRecord:
     def __init__(self, record_id, animal_id, adopter_id, adoption_date):
         self.record_id = record_id
